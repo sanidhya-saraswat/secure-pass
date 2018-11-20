2919ea90f8114e32469d10cf62980dfc46cee751
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2018-11-20 12:30:12 +0530

    x

--- back/config/keys.js
+++ back/config/keys.js
@@ -1,21 +1,11 @@
-
-if(process.env.NODE_ENV!='production')
-{
-    module.exports={
-        googleClientID:'1015478915653-bod8a4kvi8oau644cafi7bjs82g143kq.apps.googleusercontent.com',
-        googleClientSecret:'K1VhnRxjnxYsb2lbo_PMgJ9P',
-         testingURLBack:'http://192.168.0.106.nip.io:3000',
-        testingURLFront:'http://192.168.0.106:4200',
-         captchaSecret:'6Ldb4WAUAAAAAFhGJYf-KWsoW1Y3_uiwC-1uD1cR'
-    }
-}
-else
-{
-    module.exports={
-        googleClientID:'1015478915653-bod8a4kvi8oau644cafi7bjs82g143kq.apps.googleusercontent.com',
-        googleClientSecret:'K1VhnRxjnxYsb2lbo_PMgJ9P',
-             testingURLBack:'',
-        testingURLFront:'',
-         captchaSecret:'6Ldb4WAUAAAAAFhGJYf-KWsoW1Y3_uiwC-1uD1cR'
-    }
+module.exports={
+    googleClientID:'1015478915653-bod8a4kvi8oau644cafi7bjs82g143kq.apps.googleusercontent.com',
+    googleClientSecret:'K1VhnRxjnxYsb2lbo_PMgJ9P',
+/*     testingURLBack:'http://192.168.1.34.nip.io:3000',
+    testingURLFront:'http://192.168.1.34:4200',
+ */    /* testingURLBack:'http://localhost:3000',
+    testingURLFront:'http://localhost:4200',*/
+         testingURLBack:'',
+    testingURLFront:'',
+     captchaSecret:'6Ldb4WAUAAAAAFhGJYf-KWsoW1Y3_uiwC-1uD1cR'
 }
